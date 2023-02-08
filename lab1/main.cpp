f156caab3f4c76a9ee2c00857008dcdb4d1ca130
Georgy Grechko <georgre4ko@ya.ru>
2023-02-08 13:28:12 +0300

    Добавил первую лабу

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -3,15 +3,59 @@
 
 #include <iostream>
 
-void key ( GLFWwindow * window, int key, int scancode, int action, int mods )
+float degree = 0.0;
+
+void key(GLFWwindow *window, int key, int scancode, int action, int mods)
 {
-    if ( key == GLFW_KEY_ESCAPE && action == GLFW_PRESS )
-        glfwSetWindowShouldClose ( window, GL_TRUE );
+    if (action == GLFW_PRESS)
+    {
+        if (key == GLFW_KEY_ESCAPE)
+        {
+            glfwSetWindowShouldClose(window, GL_TRUE);
+        }
+        else if (key == GLFW_KEY_UP)
+        {
+            degree += 0.1;
+        }
+        else if (key == GLFW_KEY_DOWN)
+        {
+            degree -= 0.1;
+        }
+    }
+}
+
+void display(GLFWwindow *window)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glRotatef(degree * 50.f, 0.f, 0.f, 1.f);
+    glBegin(GL_POLYGON);
+    glColor3f(1.f, 0.f, 0.f);
+    glVertex3f(-0.6f, -0.4f, 0.f);
+    glColor3f(0.f, 1.f, 0.f);
+    glVertex3f(0.6f, -0.4f, 0.f);
+    glColor3f(0.f, 0.f, 1.f);
+    glVertex3f(0.f, 0.6f, 0.f);
+    glColor3f(0.f, 0.f, 1.f);
+    glVertex3f(0.f, 0.6f, 0.f);
+    glColor3f(0.f, 0.f, 1.f);
+    glVertex3f(0.4f, 0.6f, 0.f);
+    glColor3f(0.f, 0.f, 1.f);
+    glVertex3f(0.6f, 0.4f, 0.f);
+    glEnd();
+
+    glPopMatrix();
 }
 
 int main(int argc, char const *argv[])
 {
-    GLFWwindow *window = glfwCreateWindow(640, 480, "Lab 1", NULL, NULL);
+    if (!glfwInit())
+        exit(1);
+
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Lab 1", glfwGetPrimaryMonitor(), NULL);
 
     if (window == NULL)
     {
@@ -22,6 +66,13 @@ int main(int argc, char const *argv[])
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
     glfwSetKeyCallback(window, key);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        display(window);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
     glfwDestroyWindow(window);
     glfwTerminate();
